82d5c6bce3fed02393d656d2b65cfef723700056
Tiago Carreira <>
2020-12-15 09:43:32 -0300

    refactor solution for day 15

--- 2020/day-15/solver_2.py
+++ 2020/day-15/solver_2.py
@@ -5,23 +5,22 @@ import os
 curdir = os.path.dirname(os.path.realpath(__file__))
 
 
+def solution(numbers, until=2020):
+    memory = {n: i + 1 for i, n in enumerate(numbers[:-1])}
+
+    for i in range(len(numbers), until):
+        numbers.append(i - memory.get(numbers[-1], i))
+        memory[numbers[-2]] = i
+
+    return numbers[-1]
+
+
 def main():
     with open(curdir + "/input.txt") as f:
         numbers = [int(x) for x in f.readline().strip().split(",")]
 
-    memory = {n: i for i, n in enumerate(numbers[0:-1])}  # don't append last one
-
-    i = len(numbers)
-    while i < 30000000:
-        if numbers[i - 1] in memory:
-            numbers.append(i - memory[numbers[i - 1]] - 1)
-        else:
-            numbers.append(0)
-
-        memory[numbers[i - 1]] = i - 1
-        i += 1
-
-    print(f"Solution: {numbers[-1]}")
+    # print(f"Solution part 1: {solution(numbers, 2020)}")
+    print(f"Solution part 2: {solution(numbers, 30000000)}")
 
 
 if __name__ == "__main__":
