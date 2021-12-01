043eaecd081ee90e81d36253ee1a0c8fa786c7ce
Tiago Carreira <tiago.g.carreira+git@gmail.com>
2021-12-01 15:46:08 -0300

    day 01

--- 2021/README.md
+++ 2021/README.md
@@ -1,2 +1,3 @@
 # Solutions to [https://adventofcode.com/2021](https://adventofcode.com/2021)
 
+- [Day 01](day-01)
--- 2021/day-01/solver.py
+++ 2021/day-01/solver.py
@@ -5,30 +5,40 @@ from pathlib import Path
 currdir = Path(__file__).parent.absolute()
 # paste the example from the problem here ↓
 test_input = """
-1
-2
-3
+199
+200
+208
+210
+200
+207
+240
+269
+260
+263
 """.strip()
 
 def main():
     raw_input = open(currdir.joinpath("input.txt")).read()
-    raw_input = test_input  # testing with the example - comment for real input
+    # raw_input = test_input  # testing with the example
     numbers = list(map(int, raw_input.splitlines()))
 
     print("Solution to Part 1:")
     print(part1(numbers))
 
-    # print("Solution to Part 2:")
-    # print(part2(numbers))
+    print("Solution to Part 2:")
+    print(part2(numbers))
 
 
 def part1(p_input):
-    solution = p_input
-    return solution
+    counter = 0
+    for i in range(1,len(p_input)):
+        if p_input[i] > p_input[i-1]:
+            counter += 1
+    return counter
 
 def part2(p_input):
-    solution = p_input
-    return solution
+    sliding = [ sum(p_input[i-3:i]) for i in range(3,len(p_input)+1) ]
+    return part1(sliding)
 
 
 if __name__ == "__main__":
